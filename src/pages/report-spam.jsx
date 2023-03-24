4a77c167fd6b607a94f1474d9a0786de682ee89e
Steve Aldrin~ <smirksteveyt@gmail.com>
2023-03-24 23:48:29 +0530

    backend update

--- src/pages/report-spam.jsx
+++ src/pages/report-spam.jsx
@@ -1,31 +1,68 @@
 import React, { useEffect, useState } from "react";
 import { useRef } from "react";
-function Report(){
-const [selects,setSelects]=useState();
-const [value,setvalue]=useState();
-const [formError, setFormError] = useState(null);
+import supabase from "./Supabaseclient"
+function Report() {
+    const [selects, setSelects] = useState();
+    const [value, setvalue] = useState();
+    const [formError, setFormError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        if (selects == "Phone Number") {
+            const { data, error } = await supabase
+                .from('phoneno')
+                .insert([{ value }])
+
+            if (error) {
+                console.log(error)
+                setFormError('Please fill in all the fields correctly.')
+            }
+            if (data) {
+                console.log(data)
+                setFormError(null)
+
+            }
+        }
+        if (selects == "UPI ID") {
+            const { data, error } = await supabase
+                .from('upiid')
+                .insert([{ value }])
+
+            if (error) {
+                console.log(error)
+                setFormError('Please fill in all the fields correctly.')
+            }
+            if (data) {
+                console.log(data)
+                setFormError(null)
+
+            }
+        }
+    }
+
+
 
     return (
-        
+
         <div class="container slider-one-active">
-            
-            
+
+
             <div class="slider-ctr">
                 <div class="slider">
                     <form class="slider-form slider-one">
                         <h2>Report a spam</h2>
                         <label class="input">
                             <p>{selects}</p>
-                            <select  value={selects} className="dropd" onChange={e=>setSelects(e.target.value)}>
+                            <select value={selects} className="dropd" onChange={e => setSelects(e.target.value)}>
                                 <option >Phone Number</option>
                                 <option value="Email">Email</option>
                                 <option value="SMS">SMS</option>
                                 <option value="UPI ID">UPI ID</option>
                             </select>
-                       <input value={value} onChange={(e)=>setvalue(e.target.value)} className="inputarea" type="text"></input> 
+                            <input value={value} onChange={(e) => setvalue(e.target.value)} className="inputarea" type="text"></input>
                         </label>
-                    
-                        <button class="first next" >Submit</button>
+
+                        <button class="first next" onClick={handleSubmit}>Submit</button>
                     </form>
                     <form class="slider-form slider-two">
                         <h2>Are you happy with our service?</h2>
