dfa15ae1c225ca53e8877e63da913550896f1afc
Thejas Sanjeev <89839115+JazzBlaze@users.noreply.github.com>
2023-08-28 17:44:00 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react';
 import App from './App';
 import { QueryClient, QueryClientProvider} from 'react-query'
 const root = ReactDOM.createRoot(document.getElementById('root'));
